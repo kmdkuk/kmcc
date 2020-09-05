2b09bd402d6fb6c47e184b0c3573e148ffb9c061
kouki <kouworld0123@gmail.com>
2020-09-06 02:05:07 +0900

    improve error message

--- kmcc.c
+++ kmcc.c
@@ -20,6 +20,9 @@ struct Token {
   char *str;
 };
 
+// Input program
+char *user_input;
+
 Token *token;
 
 // エラーを報告する関数
@@ -27,6 +30,20 @@ Token *token;
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -44,14 +61,14 @@ bool consume(char op) {
 // それ以外の場合には，エラーを報告する．
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません．", op);
+    error_at(token->str, "'%c'ではありません．", op);
   token = token->next;
 }
 
 // 次のトークンが数値の場合，トークンを1つ読み進めてその数値を返す，
 // それ以外の場合にはエラーを報告する．
 int expect_number() {
-  if (token->kind != TK_NUM) error("数ではありません．");
+  if (token->kind != TK_NUM) error_at(token->str, "数ではありません．");
   int val = token->val;
   token = token->next;
   return val;
@@ -68,8 +85,9 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
   return tok;
 }
 
-// 入力文字列pをトークナイズしてそれを返す．
-Token *tokenize(char *p) {
+// `user_input`をトークナイズしてそれを返す．
+Token *tokenize() {
+  char *p = user_input;
   Token head;
   head.next = NULL;
   Token *cur = &head;
@@ -92,7 +110,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません．");
+    error_at(p, "トークナイズできません．");
   }
 
   new_token(TK_EOF, cur, p);
@@ -104,8 +122,8 @@ int main(int argc, char **argv) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
-
-  token = tokenize(argv[1]);
+  user_input = argv[1];
+  token = tokenize();
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
