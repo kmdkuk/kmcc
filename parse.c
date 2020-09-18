2f57a9b82bd17c869d1290764b7cc30c70a64cba
kouki <kouworld0123@gmail.com>
2020-09-18 10:01:13 +0900

    support struct tags

--- parse.c
+++ parse.c
@@ -1,19 +1,61 @@
-#include <stdlib.h>
-
 #include "kmcc.h"
+
+// struct tagsのためのスコープ
+typedef struct TagScope TagScope;
+struct TagScope {
+  TagScope *next;
+  char *name;
+  Type *ty;
+};
+
+typedef struct {
+  VarList *var_scope;
+  TagScope *tag_scope;
+} Scope;
 
 // すべてのローカル変数
 static VarList *locals;
+
+// グローバル変数のリスト
 static VarList *globals;
-static VarList *scope;
+
+// Cには2つのブロックスコープがある．
+// 変数と，構造体のタグ
+// 以下2つの変数は，現在のスコープ情報を保持
+VarList *var_scope;
+static TagScope *tag_scope;
+
+// sc は，それまでのスコープ情報を一時保存
+// leave時に，一時保存していたscをもとに戻す．
+static Scope *enter_scope() {
+  Scope *sc     = calloc(1, sizeof(Scope));
+  sc->var_scope = var_scope;
+  sc->tag_scope = tag_scope;
+  return sc;
+}
+
+static void leave_scope(Scope *sc) {
+  var_scope = sc->var_scope;
+  tag_scope = sc->tag_scope;
+}
 
 // 変数を名前で検索する．見つからなかった場合は，NULLを返す．
 static Var *find_var(Token *tok) {
-  for (VarList *vl = scope; vl; vl = vl->next) {
+  for (VarList *vl = var_scope; vl; vl = vl->next) {
     Var *var = vl->var;
     if (strlen(var->name) == tok->len
         && !strncmp(tok->str, var->name, tok->len)) {
       return var;
+    }
+  }
+  return NULL;
+}
+
+static TagScope *find_tag(Token *tok) {
+  for (TagScope *sc = tag_scope; sc; sc = sc->next) {
+    if (strlen(sc->name) == tok->len
+        && !strncmp(tok->str, sc->name, tok->len)) {
+      return sc;
     }
   }
   return NULL;
@@ -64,8 +106,8 @@ static Var *new_var(char *name, Type *ty, bool is_local) {
 
   VarList *sc = calloc(1, sizeof(VarList));
   sc->var     = var;
-  sc->next    = scope;
-  scope       = sc;
+  sc->next    = var_scope;
+  var_scope   = sc;
   return var;
 }
 
@@ -176,12 +218,31 @@ static Type *read_type_suffix(Type *base) {
   return array_of(base, array_size);
 }
 
-// struct-decl = "struct" "{" struct-member "}"
+static void push_tag_scope(Token *tok, Type *ty) {
+  TagScope *sc = calloc(1, sizeof(TagScope));
+  sc->next     = tag_scope;
+  sc->name     = duplicate(tok->str, tok->len);
+  sc->ty       = ty;
+  tag_scope    = sc;
+}
+
+// struct-decl = "struct" idnet
+//             | "struct" ident? "{" struct-member "}"
 static Type *struct_decl() {
-  // Read struct members.
   expect("struct");
+
+  // Read struct tag
+  Token *tag = consume_ident();
+  if (tag && !peek("{")) {
+    TagScope *sc = find_tag(tag);
+    if (!sc) {
+      error_tok(tag, "unknown struct type");
+    }
+    return sc->ty;
+  }
   expect("{");
 
+  // Read struct members.
   Member head = {};
   Member *cur = &head;
 
@@ -207,6 +268,10 @@ static Type *struct_decl() {
   }
   ty->size = align_to(offset, ty->align);
 
+  // nameが与えられていた場合は，struct typeを登録する．
+  if (tag) {
+    push_tag_scope(tag, ty);
+  }
   return ty;
 }
 
@@ -258,8 +323,8 @@ Function *function() {
   expect("(");
 
   // 現状のscopeを一時保存
-  VarList *sc = scope;
-  fn->params  = read_func_params();
+  Scope *sc  = enter_scope();
+  fn->params = read_func_params();
   expect("{");
 
   Node head = {};
@@ -271,7 +336,7 @@ Function *function() {
   }
   // 一時保存していたscopeを戻して,
   // 一時保存からここまでで作られた変数の情報を開放
-  scope = sc;
+  leave_scope(sc);
 
   fn->node   = head.next;
   fn->locals = locals;
@@ -288,9 +353,14 @@ static void global_var() {
 }
 
 // declartion = basetype ident ("[" num "]")* ("=" expr) ";"
+//            | basetype ";"
 static Node *declartion() {
   Token *tok = token;
   Type *ty   = basetype();
+  if (consume(";")) {
+    return new_node(ND_NULL, tok);
+  }
+
   char *name = expect_ident();
   ty         = read_type_suffix(ty);
   Var *var   = new_lvar(name, ty);
@@ -382,13 +452,13 @@ Node *stmt2() {
     Node *cur = &head;
 
     // ブロックには入る前までの変数の宣言状態を一時保存
-    VarList *sc = scope;
+    Scope *sc = enter_scope();
     while (!consume("}")) {
       cur->next = stmt();
       cur       = cur->next;
     }
     // ブロックから出たので，一時保存していた変数の宣言状態を復旧
-    scope = sc;
+    leave_scope(sc);
 
     Node *node = new_node(ND_BLOCK, tok);
     node->body = head.next;
@@ -586,7 +656,7 @@ static Node *postfix() {
 // Statement expression は， GNUCの拡張です．
 static Node *stmt_expr(Token *tok) {
   // stmt_exprに入る前までの変数の状態を一時保存
-  VarList *sc = scope;
+  Scope *sc = enter_scope();
 
   Node *node = new_node(ND_STMT_EXPR, tok);
   node->body = stmt();
@@ -599,7 +669,7 @@ static Node *stmt_expr(Token *tok) {
   expect(")");
 
   // スコープから外れる際に，一時保存していた変数の状態を復旧
-  scope = sc;
+  leave_scope(sc);
 
   if (cur->kind != ND_EXPR_STMT) {
     error_tok(
